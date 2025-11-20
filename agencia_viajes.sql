66399985e8540e716ddd8d726828d3df92075a99
aicarranza-cpu <aicarranza@tes.edu.ec>
2025-11-20 00:36:53 -0500

    Add files via upload

---
+++ agencia_viajes.sql
@@ -0,0 +1,9 @@
+INSERT INTO users (username, password_hash, full_name, email, phone, role)
+VALUES (
+    'admin',
+    '1234',
+    ' General',
+    'asronisaac49@gmail.com',
+    '0999999999',
+    'ADMIN'
+);
